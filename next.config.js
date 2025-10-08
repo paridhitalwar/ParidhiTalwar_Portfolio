0df9f32f20e188034d16e701b31ca2a1909a6fa0
Paridhi Talwar <paridhitalwar2@gmail.com>
2025-10-08 12:17:08 +0530

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,14 +4,15 @@ const repositoryName = 'ParidhiTalwar_Portfolio';
 
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
+  distDir: 'out',
   basePath: isProd ? `/${repositoryName}` : '',
   assetPrefix: isProd ? `/${repositoryName}/` : '',
   images: {
     unoptimized: true,
   },
-  // Optional: Add this if you have any API routes that need to be handled
   trailingSlash: true,
+  // Optional: Add this if you have any API routes that need to be handled
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
